7189e1c6ab9bf0eac1cd0f78e238b68437f93030
chandlerkeyes <ckeyes4@emich.edu>
2017-10-11 10:39:14 -0400

    first and only commit

---
+++ Loops.java
@@ -0,0 +1,20 @@
+package com.jdbc.Practice;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        int x = 10;
+//        while (x > 9) {
+//            System.out.println("Test Worked!");
+//            x--;
+//        }
+
+//        do {
+//            System.out.println("Test worked!");
+//            x--;
+//        }while (x < 10);
+//        for (int i = 1; i < x; i++){
+//            System.out.println("Test worked");
+//        }
+    }
+}
